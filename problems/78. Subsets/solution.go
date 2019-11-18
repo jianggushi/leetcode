877c4b6d1c4a482c1f8118cc2aba0e49bc7c436d
jianggushi <rogerjiangx@outlook.com>
2019-11-18 09:47:06 +0800

    update problem 78

--- problems/78. Subsets/solution.go
+++ problems/78. Subsets/solution.go
@@ -1,11 +1,33 @@
 package solution
 
+// 回溯 - 递归
 func subsets(nums []int) [][]int {
+	ans := make([][]int, 0)
+	backtrack(nums, 0, []int{}, &ans)
+	return ans
+}
+
+func backtrack(nums []int, first int, curr []int, ans *[][]int) {
+	tmp := make([]int, len(curr))
+	copy(tmp, curr)
+	*ans = append(*ans, tmp)
+	for i := first; i < len(nums); i++ {
+		curr = append(curr, nums[i])
+		backtrack(nums, i+1, curr, ans)
+		curr = curr[:len(curr)-1]
+	}
+}
+
+// 迭代
+func subsets1(nums []int) [][]int {
 	n := len(nums)
+	// 结果列表
 	ans := make([][]int, 1)
 	ans[0] = []int{}
+	// 遍历每一个数字
 	for i := 0; i < n; i++ {
 		m := len(ans)
+		// 和结果列表中的组合，形成新的结果并加到结果列表
 		for j := 0; j < m; j++ {
 			tmp := make([]int, len(ans[j])+1)
 			copy(tmp, ans[j])
--- problems/78. Subsets/solution_test.go
+++ problems/78. Subsets/solution_test.go
@@ -5,20 +5,52 @@ import (
 	"testing"
 )
 
+func Equal(a [][]int, b [][]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	n := len(a)
+	// a 中的元素在 b 中都可以找到
+	for i := 0; i < n; i++ {
+		find := false
+		for j := 0; j < n; j++ {
+			if reflect.DeepEqual(a[i], b[j]) {
+				find = true
+			}
+		}
+		if !find {
+			return false
+		}
+	}
+	// b 中的元素在 a 中都可以找到
+	for i := 0; i < n; i++ {
+		find := false
+		for j := 0; j < n; j++ {
+			if reflect.DeepEqual(b[i], a[j]) {
+				find = true
+			}
+		}
+		if !find {
+			return false
+		}
+	}
+	return true
+}
+
 func Test_subsets_1(t *testing.T) {
 	nums := []int{1, 2, 3}
 	expected := [][]int{
-		[]int{},
-		[]int{1},
-		[]int{2},
-		[]int{1, 2},
-		[]int{3},
-		[]int{1, 3},
-		[]int{2, 3},
-		[]int{1, 2, 3},
+		{},
+		{1},
+		{2},
+		{1, 2},
+		{3},
+		{1, 3},
+		{2, 3},
+		{1, 2, 3},
 	}
 	ans := subsets(nums)
-	if !reflect.DeepEqual(ans, expected) {
+	if !Equal(ans, expected) {
 		t.Errorf("expected: %v, subsets: %v", expected, ans)
 	}
 }
@@ -26,17 +58,17 @@ func Test_subsets_1(t *testing.T) {
 func Test_subsets_2(t *testing.T) {
 	nums := []int{9, 0, 3, 5, 7}
 	expected := [][]int{
-		[]int{},
-		[]int{1},
-		[]int{2},
-		[]int{1, 2},
-		[]int{3},
-		[]int{1, 3},
-		[]int{2, 3},
-		[]int{1, 2, 3},
+		{},
+		{1},
+		{2},
+		{1, 2},
+		{3},
+		{1, 3},
+		{2, 3},
+		{1, 2, 3},
 	}
 	ans := subsets(nums)
-	if !reflect.DeepEqual(ans, expected) {
+	if !Equal(ans, expected) {
 		t.Errorf("expected: %v, subsets: %v", expected, ans)
 	}
 }
