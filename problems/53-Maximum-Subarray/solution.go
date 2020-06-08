5ec195639b29dcfea10586f0da70b9608953d116
jianggushi <rogerjiangx@outlook.com>
2020-06-08 08:02:39 +0800

    add problem 53

--- problems/53-Maximum-Subarray/solution.go
+++ problems/53-Maximum-Subarray/solution.go
@@ -1,15 +1,6 @@
 package solution
 
-// 53. Maximum Subarray
-// https://leetcode.com/problems/maximum-subarray/
-
-// Given an integer array nums, find the contiguous subarray (containing at
-// least one number) which has the largest sum and return its sum.
-
-// Example:
-// Input: [-2,1,-3,4,-1,2,1,-5,4],
-// Output: 6
-// Explanation: [4,-1,2,1] has the largest sum = 6.
+import "math"
 
 func maxSubArray1(nums []int) int {
 	n := len(nums)
@@ -34,17 +25,19 @@ func sum(nums []int) int {
 	return total
 }
 
+// 暴力解法
+// 遍历所有可能的连续子数组
 func maxSubArray(nums []int) int {
 	n := len(nums)
-	result := nums[0]
+	ans := math.MinInt64
 	for i := 0; i < n; i++ {
 		tmp := 0
 		for j := i; j < n; j++ {
 			tmp += nums[j]
-			if tmp > result {
-				result = tmp
+			if tmp > ans {
+				ans = tmp
 			}
 		}
 	}
-	return result
+	return ans
 }
