483e49ddbca58dc43b4348f0666f36adab093f17
jianggushi <rogerjiangx@outlook.com>
2020-10-13 22:45:50 +0800

    feat: add 24. Swap Nodes in Pairs

--- problems/24-Swap-Nodes-in-Pairs/solution.go
+++ problems/24-Swap-Nodes-in-Pairs/solution.go
@@ -1,23 +1,10 @@
 package solution
 
-// Given a linked list, swap every two adjacent nodes and return its head.
-// You may not modify the values in the list's nodes, only nodes itself may be changed.
-//
-// Example 1:
-// Given 1->2->3->4, you should return the list as 2->1->4->3.
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func swapPairs(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
